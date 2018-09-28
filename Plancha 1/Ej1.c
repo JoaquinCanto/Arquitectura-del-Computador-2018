4647135a028522fc599558f4e6d54b7f821cfc72
Joaquín <37853387+Virdian@users.noreply.github.com>
2018-09-28 12:08:10 -0300

    Ej 1

---
+++ Plancha 1/Ej1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+printb(int n){
+  int i;
+  for(i = sizeof(int)*8 - 1; i >= 0; i--){
+    if(n & 1 << i) printf("1");
+    else printf("0");
+    if(i%8 == 0) printf(" ");
+  }
+  puts("");
+}
+
+int main(){
+  int a = 1 << 31;
+  int b = (1 << 31) | (1 << 15);
+  int c = -1 & 0xFFFFFF00;
+  int d = 0xAA | (0xAA << 24);
+  int e = 5 << 100;
+  int f = -1 & (... (1 << 8));
+
+  printb(a);
+  printb(b);
+  printb(c);
+  printb(d);
+  printb(e);
+  //printb(f);
+
+  return 0;
+}
